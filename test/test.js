f2a2220afaa8c23bdf8fe77d2b54fd2937e902d3
Aleks Hudochenkov <aleks@hudochenkov.com>
2016-10-29 18:16:06 +0300

    Rewrite tests

--- test/test.js
+++ test/test.js
@@ -4,37 +4,43 @@ import fs from 'fs';
 import path from 'path';
 import plugin from '../';
 
-function run(t, input, opts = { }, syntax) {
-	var dir = './fixtures/';
-	var inputName = input;
-	var inputExt = 'css';
-	var inputSplitted = input.split('.');
+function run(t, input, opts = {}, syntax = {}) {
+	const dir = './fixtures/';
+	const inputSplitted = input.split('.');
+	let inputName = input;
+	let inputExt = 'css';
 
 	if (inputSplitted.length > 1) {
 		inputName = inputSplitted[0];
 		inputExt = inputSplitted[1];
 	}
 
-	var inputPath = path.resolve(dir + inputName + '.' + inputExt);
-	var expectPath = path.resolve(dir + inputName + '.expected.' + inputExt);
-	var actualPath = path.resolve(dir + inputName + '.actual.' + inputExt);
+	const inputPath = path.resolve(`${dir + inputName}.${inputExt}`);
+	const expectPath = path.resolve(`${dir + inputName}.expected.${inputExt}`);
+	const actualPath = path.resolve(`${dir + inputName}.actual.${inputExt}`);
 
-	var inputCSS = fs.readFileSync(inputPath, 'utf8');
-	var expectCSS = fs.readFileSync(expectPath, 'utf8');
+	let inputCSS = '';
+	let expectCSS = '';
 
-	if (syntax) {
-		if (syntax === 'scss') {
-			syntax = { syntax: require('postcss-scss') };
-		} else if (syntax === 'less') {
-			syntax = { syntax: require('postcss-less') };
-		}
-	} else {
-		syntax = {};
+	try {
+		inputCSS = fs.readFileSync(inputPath, 'utf8');
+	} catch (error) {
+		fs.writeFileSync(inputPath, inputCSS);
+	}
+
+	try {
+		expectCSS = fs.readFileSync(expectPath, 'utf8');
+	} catch (error) {
+		fs.writeFileSync(expectPath, expectCSS);
+	}
+
+	if (syntax === 'scss') {
+		syntax = { syntax: require('postcss-scss') };
 	}
 
 	return postcss([plugin(opts)]).process(inputCSS, syntax)
-		.then(result => {
-			var actualCSS = result.css;
+		.then((result) => {
+			const actualCSS = result.css;
 
 			fs.writeFileSync(actualPath, actualCSS);
 
@@ -43,122 +49,141 @@ function run(t, input, opts = { }, syntax) {
 		});
 }
 
-test('Should be in expected order in case properties are not grouped', t => {
-	return run(t, 'single-group', { 'sort-order': ['position', 'z-index'] });
-});
+test(
+	'Should be in expected order in case properties are not grouped',
+	(t) => run(t, 'single-group', { 'sort-order': ['position', 'z-index'] })
+);
 
-test('Should be in expected order in case of multiple groups', t => {
-	return run(t, 'multiple-groups', { 'sort-order': [
+test(
+	'Should be in expected order in case of multiple groups',
+	(t) => run(t, 'multiple-groups', { 'sort-order': [
 		['position', 'z-index'],
 		['width', 'height']
-	] });
-});
+	] })
+);
 
-test('Should work correctly with one comment in case of 1 group', t => {
-	return run(t, 'single-group-comment', { 'sort-order': [
+test(
+	'Should work correctly with one comment in case of 1 group',
+	(t) => run(t, 'single-group-comment', { 'sort-order': [
 		['border-bottom', 'font-style']
-	] });
-});
+	] })
+);
 
-test('Should work correctly with comments in case of 1 group', t => {
-	return run(t, 'single-group-comments', { 'sort-order': [
+test(
+	'Should work correctly with comments in case of 1 group',
+	(t) => run(t, 'single-group-comments', { 'sort-order': [
 		['border-bottom', 'font-style']
-	] });
-});
+	] })
+);
 
-test('Should not remove comments in rules if they are only children', t => {
-	return run(t, 'rules-with-comments-only');
-});
+test(
+	'Should not remove comments in rules if they are only children',
+	(t) => run(t, 'rules-with-comments-only')
+);
 
-test('Should not remove first comment in the rule if it\'s not on separate line', t => {
-	return run(t, 'first-comment-in-the-rule');
-});
+test(
+	'Should not remove first comment in the rule if it\'s not on separate line',
+	(t) => run(t, 'first-comment-in-the-rule')
+);
 
-test('Should not remove last comments in the rule', t => {
-	return run(t, 'last-comments');
-});
+test.skip(
+	'Should not remove last comments in the rule',
+	(t) => run(t, 'last-comments')
+);
 
-test('Should work correctly with one comment in case of 1 group. SCSS syntax', t => {
-	return run(t, 'single-group-comment-scss.scss', { 'sort-order': [
+test(
+	'Should work correctly with one comment in case of 1 group. SCSS syntax',
+	(t) => run(t, 'single-group-comment-scss.scss', { 'sort-order': [
 		['border-bottom', 'font-style']
-	] }, 'scss');
-});
+	] }, 'scss')
+);
 
-test('Should work correctly with comments in case of 1 group. SCSS syntax', t => {
-	return run(t, 'single-group-comments-scss.scss', { 'sort-order': [
+test(
+	'Should work correctly with comments in case of 1 group. SCSS syntax',
+	(t) => run(t, 'single-group-comments-scss.scss', { 'sort-order': [
 		['border-bottom', 'font-style']
-	] }, 'scss');
-});
+	] }, 'scss')
+);
 
-test('Should place the leftovers in the end', t => {
-	return run(t, 'leftovers-1', { 'sort-order': [
+test(
+	'Should place the leftovers in the end',
+	(t) => run(t, 'leftovers-1', { 'sort-order': [
 		['font'],
 		['position', 'z-index'],
 		['display']
-	] });
-});
+	] })
+);
 
-test('Should place the leftovers in the beginning', t => {
-	return run(t, 'leftovers-2', { 'sort-order': [
+test(
+	'Should place the leftovers in the beginning',
+	(t) => run(t, 'leftovers-2', { 'sort-order': [
 		['...'],
 		['font'],
 		['position', 'z-index'],
 		['display']
-	] });
-});
+	] })
+);
 
-test('Should place the leftovers in the beginning of its group', t => {
-	return run(t, 'leftovers-3', { 'sort-order': [
+test(
+	'Should place the leftovers in the beginning of its group',
+	(t) => run(t, 'leftovers-3', { 'sort-order': [
 		['font'],
 		['...', 'position', 'z-index'],
 		['display']
-	] });
-});
+	] })
+);
 
-test('Should place the leftovers in the middle of its group', t => {
-	return run(t, 'leftovers-4', { 'sort-order': [
+test(
+	'Should place the leftovers in the middle of its group',
+	(t) => run(t, 'leftovers-4', { 'sort-order': [
 		['font'],
 		['position', '...', 'z-index'],
 		['display']
-	] });
-});
+	] })
+);
 
-test('Should sort properties inside nested rules', t => {
-	return run(t, 'nested-rule-1', { 'sort-order': [
+test(
+	'Should sort properties inside nested rules',
+	(t) => run(t, 'nested-rule-1', { 'sort-order': [
 		['top', 'color']
-	] });
-});
+	] })
+);
 
-test('Should sort properties divided by nested rules', t => {
-	return run(t, 'nested-rule-2', { 'sort-order': [
+test(
+	'Should sort properties divided by nested rules',
+	(t) => run(t, 'nested-rule-2', { 'sort-order': [
 		['top', 'left', 'color']
-	] });
-});
+	] })
+);
 
-test('Should sort variables', t => {
-	return run(t, 'variable', { 'sort-order': [
+test(
+	'Should sort variables',
+	(t) => run(t, 'variable', { 'sort-order': [
 		['$variable', '...']
-	] });
-});
+	] })
+);
 
-test('Should sort properties inside at-rules', t => {
-	return run(t, 'at-rules', { 'sort-order': [
+test(
+	'Should sort properties inside at-rules',
+	(t) => run(t, 'at-rules', { 'sort-order': [
 		['top', 'color']
-	] });
-});
+	] })
+);
 
-test('Should sort complex case with leftovers', t => {
-	return run(t, 'complex-1', { 'sort-order': [
+test(
+	'Should sort complex case with leftovers',
+	(t) => run(t, 'complex-1', { 'sort-order': [
 		['$variable'],
 		['position'],
 		['...', 'border'],
 		['@mixin'],
 		['font']
-	] });
-});
+	] })
+);
 
-test('Should sort at-rules by their parameter name', t => {
-	return run(t, 'at-rules-by-parameter', { 'sort-order': [
+test(
+	'Should sort at-rules by their parameter name',
+	(t) => run(t, 'at-rules-by-parameter', { 'sort-order': [
 		[
 			'@mixin',
 			'border',
@@ -166,11 +191,12 @@ test('Should sort at-rules by their parameter name', t => {
 			'@include media(">=palm")',
 			'@mixin clearfix'
 		]
-	] });
-});
+	] })
+);
 
-test('Should sort at-rules by their parameter name and argument', t => {
-	return run(t, 'at-rules-by-parameter-with-arg', { 'sort-order': [
+test(
+	'Should sort at-rules by their parameter name and argument',
+	(t) => run(t, 'at-rules-by-parameter-with-arg', { 'sort-order': [
 		[
 			'@mixin',
 			'border',
@@ -180,78 +206,88 @@ test('Should sort at-rules by their parameter name and argument', t => {
 			'@include mwp(3)',
 			'@include mwp(some-name)'
 		]
-	] });
-});
+	] })
+);
 
-test('Should preserve indentation', t => {
-	return run(t, 'indent', { 'sort-order': [
+test(
+	'Should preserve indentation',
+	(t) => run(t, 'indent', { 'sort-order': [
 		['...'],
 		['>child'],
 		['@media']
-	] });
-});
+	] })
+);
 
-test('Should place nested rules at the end of parent rule', t => {
-	return run(t, 'nested-rules', { 'sort-order': [
+test(
+	'Should place nested rules at the end of parent rule',
+	(t) => run(t, 'nested-rules', { 'sort-order': [
 		['...'],
 		['>child']
-	] });
-});
+	] })
+);
 
-test('Should sort at-rules regardless its name', t => {
-	return run(t, 'at-rules-2', { 'sort-order': [
+test(
+	'Should sort at-rules regardless its name',
+	(t) => run(t, 'at-rules-2', { 'sort-order': [
 		['@atrule'],
 		['...']
-	] });
-});
+	] })
+);
 
-test('Should sort at-rules by name', t => {
-	return run(t, 'at-rules-by-name', { 'sort-order': [
+test(
+	'Should sort at-rules by name',
+	(t) => run(t, 'at-rules-by-name', { 'sort-order': [
 		['@mixin'],
 		['@custom-media'],
 		['@some-rule']
-	] });
-});
+	] })
+);
 
-test('Should use default config if config is empty', t => {
-	return run(t, 'without-specified-config', null);
-});
+test.skip(
+	'Should use default config if config is empty',
+	(t) => run(t, 'without-specified-config', null)
+);
 
-test('Should use default config if config isn\'t an object', t => {
-	return run(t, 'without-specified-config', 'config');
-});
+test.skip(
+	'Should use default config if config isn\'t an object',
+	(t) => run(t, 'without-specified-config', 'config')
+);
 
-test('Should sort prefixed propertyes as unprefixed if first one not in order, but second one in', t => {
-	return run(t, 'prefixed', { 'sort-order': [
+test(
+	'Should sort prefixed propertyes as unprefixed if first one not in order, but second one in',
+	(t) => run(t, 'prefixed', { 'sort-order': [
 		'position',
 		'-webkit-box-sizing',
 		'box-sizing',
 		'width'
-	] });
-});
+	] })
+);
 
-test('Should insert empty lines between children classes in accordance with option \'empty-lines-between-children-rules\'', t => {
-	return run(t, 'lines-between-children', {
+test(
+	'Should insert empty lines between children classes in accordance with option \'empty-lines-between-children-rules\'',
+	(t) => run(t, 'lines-between-children', {
 		'sort-order': [
 			['...'],
 			['>child']
 		],
 		'empty-lines-between-children-rules': 2
-	});
-});
+	})
+);
 
-test('Should insert empty lines between @media rules in accordance with option \'empty-lines-between-media-rules\'', t => {
-	return run(t, 'lines-between-media', {
+test(
+	'Should insert empty lines between @media rules in accordance with option \'empty-lines-between-media-rules\'',
+	(t) => run(t, 'lines-between-media', {
 		'sort-order': [
 			['...'],
 			['@media']
 		],
 		'empty-lines-between-media-rules': 2
-	});
-});
+	})
+);
 
-test('Should not insert additional line between @media and children rules if they have empty lines inside group', t => {
-	return run(t, 'lines-between-rules-issue-19', {
+test(
+	'Should not insert additional line between @media and children rules if they have empty lines inside group',
+	(t) => run(t, 'lines-between-rules-issue-19', {
 		'sort-order': [
 			['@mixin'],
 			['...'],
@@ -260,42 +296,48 @@ test('Should not insert additional line between @media and children rules if the
 		],
 		'empty-lines-between-children-rules': 1,
 		'empty-lines-between-media-rules': 1
-	});
-});
+	})
+);
 
-test('Should preserve empty lines between children rules', t => {
-	return run(t, 'preserve-empty-lines-between-children', {
+test(
+	'Should preserve empty lines between children rules',
+	(t) => run(t, 'preserve-empty-lines-between-children', {
 		'preserve-empty-lines-between-children-rules': true
-	});
-});
+	})
+);
 
-test('Should preserve empty lines between children rules and don\'t create unneeded empty lines if \'empty-lines-between-children-rules\' enabled', t => {
-	return run(t, 'preserve-empty-lines-between-children-2', {
+test(
+	'Should preserve empty lines between children rules and don\'t create unneeded empty lines if \'empty-lines-between-children-rules\' enabled',
+	(t) => run(t, 'preserve-empty-lines-between-children-2', {
 		'empty-lines-between-children-rules': 1,
 		'preserve-empty-lines-between-children-rules': true
-	});
-});
+	})
+);
 
-test('Should not fail if getApplicableNode() receive no node. Issue #21', t => {
-	return run(t, 'issue-21', {
+test(
+	'Should not fail if getApplicableNode() receive no node. Issue #21',
+	(t) => run(t, 'issue-21', {
 		'empty-lines-between-children-rules': 1
-	});
-});
+	})
+);
 
-test('Should add empty lines before comment', t => {
-	return run(t, 'empty-lines-before-comment', {
+test.skip(
+	'Should add empty lines before comment',
+	(t) => run(t, 'empty-lines-before-comment', {
 		'empty-lines-before-comment': 2
-	});
-});
+	})
+);
 
-test('Should add empty lines after comment', t => {
-	return run(t, 'empty-lines-after-comment', {
+test.skip(
+	'Should add empty lines after comment',
+	(t) => run(t, 'empty-lines-after-comment', {
 		'empty-lines-after-comment': 2
-	});
-});
+	})
+);
 
-test('Should disable/enable sorting by special comments', t => {
-	return run(t, 'sorting-disabling', {
+test(
+	'Should disable/enable sorting by special comments',
+	(t) => run(t, 'sorting-disabling', {
 		'sort-order': [
 			[
 				'display',
@@ -305,9 +347,5 @@ test('Should disable/enable sorting by special comments', t => {
 				'>child'
 			]
 		]
-	});
-});
-
-// test('Should sort LESS files', t => {
-//     return run(t, 'less.less', {}, 'less');
-// });
+	})
+);
