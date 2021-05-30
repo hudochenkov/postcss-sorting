ac1572335528fcba5aad824bfbbe52b3f289dba5
Aleks Hudochenkov <aleks@hudochenkov.com>
2021-05-31 01:33:46 +0200

    Remove lodash from dependencies

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const _ = require('lodash');
-
 const order = require('./lib/order');
 const propertiesOrder = require('./lib/properties-order');
 const validateOptions = require('./lib/validateOptions');
+const isString = require('./lib/isString');
 
 module.exports = (opts) => {
 	return {
@@ -19,17 +18,17 @@ function plugin(css, opts) {
 	const validatedOptions = validateOptions(opts);
 
 	if (validatedOptions !== true) {
-		const throwValidateErrors = _.get(opts, 'throw-validate-errors', false);
+		const throwValidateErrors = (opts && opts['throw-validate-errors']) || false;
 
 		if (throwValidateErrors) {
-			if (_.isString(validatedOptions)) {
+			if (isString(validatedOptions)) {
 				throw new Error(validatedOptions);
 			}
 
 			throw new Error(`postcss-sorting: Invalid config.`);
 		} else {
 			// eslint-disable-next-line no-console
-			if (console && console.warn && _.isString(validatedOptions)) {
+			if (console && console.warn && isString(validatedOptions)) {
 				console.warn(validatedOptions); // eslint-disable-line no-console
 			}
 
